0549784bc59c2ad8e7e80a519101f6318d346297
Edwin Siby <69618450+edzy16@users.noreply.github.com>
2020-08-26 09:18:44 +0530

    Add files via upload

---
+++ mad libs generator.py
@@ -0,0 +1,17 @@
+#The program design is such that it will ask users to enter a series of inputs that will be considered as a Mad Lib.
+#The input could be anything, an adjective, a noun, a pronoun, etc. Once all the inputs are entered, the application will take the data and arrange the inputs into a story template form. 
+
+name = input ("name\t")
+email = input ("email\t")
+familyname = input ("family name\t")
+place = input ("place\t")
+zipcode = int (input ("zipcode\t"))
+mobile = int (input ("mobile\t"))
+def address() :
+    name
+    email
+    familyname
+    place
+    zipcode
+    mobile 
+print ( "Address =" +address() )
